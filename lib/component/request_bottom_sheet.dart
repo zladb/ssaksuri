009334be701bc38169639a87660846d329b3f722
zladb <zla_db@naver.com>
2023-08-24 00:23:06 +0900

    마이페이지 추가
    ㅎㅎ

--- lib/component/request_bottom_sheet.dart
+++ lib/component/request_bottom_sheet.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ssaksuri/const/colors.dart';
 
-class RequestBottomSheet extends StatelessWidget {
+class RequestBottomSheet extends StatefulWidget {
   final String category;
   final String item_label;
 
@@ -12,7 +16,69 @@ class RequestBottomSheet extends StatelessWidget {
   });
 
   @override
+  State<RequestBottomSheet> createState() => _RequestBottomSheetState();
+}
+
+class _RequestBottomSheetState extends State<RequestBottomSheet> {
+  final ImagePicker _picker = ImagePicker();
+  List<XFile> _pickedImgs = [];
+  Map<String, String> reponse = {};
+  DateTime date = DateTime.now();
+  final dateController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // dateController.addListener();
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    dateController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    List<Widget> _boxContents = [
+      IconButton(
+        onPressed: () {
+          _pickImg();
+        },
+        icon: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.6), shape: BoxShape.circle),
+          child: Icon(
+            CupertinoIcons.camera,
+            color: primaryColor,
+          ),
+        ),
+      ),
+      Container(),
+      Container(),
+      _pickedImgs.length <= 4
+          ? Container()
+          : FittedBox(
+              child: Container(
+                padding: EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.6),
+                    shape: BoxShape.circle),
+                child: Text(
+                  '+${(_pickedImgs.length - 4).toString()}',
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+            )
+    ];
+
     final bottomInset =
         MediaQuery.of(context).viewInsets.bottom; // 시스템 ui 만큼 가려진 크기
 
@@ -41,7 +107,7 @@ class RequestBottomSheet extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Text(
-                '$category 수거 정보 입력',
+                '${widget.category} 수거 정보 입력',
                 style: TextStyle(
                   fontWeight: FontWeight.w700,
                   fontSize: 20,
@@ -58,10 +124,35 @@ class RequestBottomSheet extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 5),
-              Container(
+              // Container(
+              //   height: 100,
+              //   width: 100,
+              //   color: primaryColor,
+              // ),
+              SizedBox(
                 height: 100,
-                width: 100,
-                color: primaryColor,
+                child: GridView.count(
+                  padding: EdgeInsets.all(2),
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                  children: List.generate(
+                    4,
+                    (index) => Container(
+                      child: Center(
+                        child: _boxContents[index],
+                      ),
+                      decoration: index <= _pickedImgs.length - 1
+                          ? BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image:
+                                      FileImage(File(_pickedImgs[index].path))))
+                          : BoxDecoration(color: Colors.white),
+                    ),
+                  ),
+                ),
               ),
               SizedBox(height: 5),
               Expanded(
@@ -70,7 +161,7 @@ class RequestBottomSheet extends StatelessWidget {
                   children: [
                     renderTextFiled(label: '제품명'),
                     renderTextFiled(label: '수거 희망 주소'),
-                    renderTextFiled(label: '수거 희망 날짜'),
+                    renderDateFiled(label: '수거 희망 날짜'),
                     SizedBox(height: 45),
                     renderMileage(context),
                   ],
@@ -86,6 +177,14 @@ class RequestBottomSheet extends StatelessWidget {
 
   TextField renderTextFiled({required String label}) {
     return TextField(
+      onChanged: (text) {
+        setState(
+          () {
+            reponse.addAll({label: text});
+            print(reponse);
+          },
+        );
+      },
       cursorColor: primaryColor,
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
@@ -100,9 +199,62 @@ class RequestBottomSheet extends StatelessWidget {
     );
   }
 
+  TextField renderDateFiled({required String label}) {
+    return TextField(
+      controller: dateController,
+      onChanged: (text) {
+        dateController.text = date.toString();
+        setState(
+          () {
+
+            reponse.addAll({label: text});
+            print(reponse);
+          },
+        );
+      },
+      cursorColor: primaryColor,
+      decoration: InputDecoration(
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: primaryColor),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: darkColor),
+        ),
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.black87),
+        suffixIcon: IconButton(
+          onPressed: () async {
+            final selectedDate = await showDatePicker(
+              context: context,
+              initialDate: date,
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2100),
+              builder: (BuildContext context, Widget? child) {
+                return Theme(
+                  data: ThemeData.dark(), //다크 테마
+                  child: child!,
+                );
+              },
+            );
+            if (selectedDate != null) {
+              setState(() {
+                date = selectedDate;
+              });
+            }
+            print('{$date.year}');
+          },
+          icon: Icon(Icons.calendar_month),
+        ),
+        suffixIconColor: primaryColor,
+      ),
+    );
+  }
+
   ElevatedButton renderRequestButton() {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.resolveWith(
           (Set<MaterialState> states) {
@@ -125,11 +277,13 @@ class RequestBottomSheet extends StatelessWidget {
     );
   }
 
-  Widget renderMileage(context){
+  Widget renderMileage(context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        SizedBox(height: 10,),
+        SizedBox(
+          height: 10,
+        ),
         Text(
           '  예상 마일리지',
           style: TextStyle(
@@ -139,7 +293,9 @@ class RequestBottomSheet extends StatelessWidget {
           ),
           textAlign: TextAlign.left,
         ),
-        SizedBox(height: 10,),
+        SizedBox(
+          height: 10,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
@@ -176,4 +332,14 @@ class RequestBottomSheet extends StatelessWidget {
       ],
     );
   }
+
+  Future<void> _pickImg() async {
+    final List<XFile>? images = await _picker.pickMultiImage();
+    if (images != null) {
+      setState(() {
+        _pickedImgs = images;
+      });
+    }
+  }
+
 }
---
+++ lib/utils/image_upload.dart
@@ -0,0 +1,17 @@
+// import 'dart:io';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:flutter/material.dart';
+//
+// class ImageUpload{
+//   final ImagePicker _picker = ImagePicker();
+//   List<XFile> _pickedImgs = [];
+//
+//   Future<void> _pickImg() async {
+//     final List<XFile>? images = await _picker.pickMultiImage();
+//     if(images != null){
+//       setState((){
+//         _pickedImgs = images;
+//       });
+//     }
+// }
+//
